76d04cea16b89eb19a571372c06521203f2e4526
erka8345 <eric.kaufmann@tu-ilmenau.de>
2021-08-30 19:23:55 +0200

    add form

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 https://github.com/ushelp/EasyQRCode-React-Native
 https://docs.expo.dev/versions/latest/sdk/bar-code-scanner/#supported-formats
+https://react-hook-form.com/get-started
--- screens/CreateNewPlantScreen.tsx
+++ screens/CreateNewPlantScreen.tsx
@@ -1,16 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { Platform, StyleSheet } from 'react-native';
-import { Title, Card, Paragraph, Button, } from "react-native-paper";
+import { Platform, StyleSheet, TextInput, Button } from 'react-native';
+import { Title, Card, Paragraph,  } from "react-native-paper";
+import { useForm, Controller } from "react-hook-form";
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View, } from '../components/Themed';
 
+type Inputs = {
+  example: string,
+  exampleRequired: string,
+};
+
 export default function CreateNewPlantScreen({ navigation }: any) {
+  const { control, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+  // const onSubmit = (data: any) => alert(`data ${data}`);
+
   return (
     <View style={styles.container}>
-      <Text>Blablabla new plant</Text>
-      {/* ADD FORM HERE */}
+        {/* ADD FORM HERE */}
+      <Controller
+          control={control}
+          rules={{
+          required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={{backgroundColor: '#C0C0C0'}}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}/>
+          )}
+          name="firstName"
+          defaultValue=""/>
+
+      {errors.firstName && <Text>This is required.</Text>}
+
+      <Controller
+        control={control}
+        rules={{
+          maxLength: 100,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={{backgroundColor: '#C0C0C0'}}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}/>
+        )}
+        name="lastName"
+        defaultValue=""/>
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)}/>
     </View>
   );
 }
